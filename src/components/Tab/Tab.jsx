808e12115f18cd87f7cc7af68ec029887d7440b7
Ruslan Nuriev <cmetall@rambler.ru>
2024-07-15 16:27:40 +0300

    fix

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -1,10 +1,6 @@
 import Button from "../Button/Button";
 import classes from "./Tab.module.css";
 
-// export default function Tab({ obj, changeTabHandler, text }) {
-//   return <Button onClick={() => changeTabHandler(obj)}>{text}</Button>;
-// }
-
 export default function Tab({ changeTabHandler, id, text }) {
   return <Button onClick={() => changeTabHandler(id)}>{text}</Button>;
 }
